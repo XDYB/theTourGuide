950058bdb6d015ba2d523effbdf8fdb5dd4c713c
yesanqiu <50287209+yesanqiu@users.noreply.github.com>
2020-02-19 20:19:29 +0800

    init

---
+++ src/main/java/com/ysq/theTourGuide/config/constants/AliyunOSSConfigConstant.java
@@ -0,0 +1,17 @@
+package com.ysq.theTourGuide.config.constants;
+
+public class AliyunOSSConfigConstant {
+    //私有构造方法 禁止该类初始化
+    private AliyunOSSConfigConstant() {
+    }
+
+    //仓库名称
+    public static final String BUCKE_NAME = "csrj";
+    //地域节点
+    public static final String END_POINT = "oss-cn-shenzhen.aliyuncs.com";
+    //AccessKey ID
+    public static final String AccessKey_ID = "LTAI4Fm5nh2XdApj1Fa1sFkY";
+    //Access Key Secret
+    public static final String AccessKey_Secret = "ivjGUOuc6LIIN9bD73XRsqKtjjV5YS";
+
+}
